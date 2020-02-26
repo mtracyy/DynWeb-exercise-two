410a9eb90020611ef1c47ed24cb48507f1694e38
mtracyy <mtracy.vp@gmail.com>
2020-02-26 09:31:55 -0500

    install axios

--- exercise-two/src/containers/Home.js
+++ exercise-two/src/containers/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+//API Key
+const defaultkey = "98387b5ecfc7a23f50ccd2becb2faa10";
+
 function Home() {
+
     return (
         <div>
+            <h2>Home</h2>
         </div>
-    )
+    );
 
 }
